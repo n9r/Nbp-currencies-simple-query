0d760e48e704b44936ef4c6c037b4d8b252e292e
n9r <no9org@gmail.com>
2016-08-03 21:08:34 +0200

    More cleanup.

--- js/mechanics.js
+++ js/mechanics.js
@@ -1,27 +1,20 @@
 var nbpCurrApp = angular.module("nbpCurrApp", []);
 
 nbpCurrApp.controller("currDispCtrl", function ($scope, $http, $filter){
-	$scope.readyMessage = "Working.";
 	$scope.nbpJson = "";
-	
 	$scope.fromDate = $filter('date')(new Date(),'yyyy-MM-dd');
 	$scope.toDate = $filter('date')(new Date(),'yyyy-MM-dd');
-
 	$scope.getData = function () {
-
 		angular.element(document.querySelector('#info')).addClass('bg-info');
-		
 		if ($scope.getUrl === undefined) {
 			$scope.getUrl = ""
 		} else {		
 			$scope.getUrl = "http://api.nbp.pl/api/exchangerates/tables/a/"+$scope.fromDate+"/"+$scope.toDate+"/?format=json";
 		}
-
 		$scope.promise = $http({method: "GET", url:	$scope.getUrl, responseType: "json"});
 		$scope.promise.then( function(data){
 		$scope.nbpJson = data;
 		});
-
 		$scope.promise
 			.then(
 				function(response) { 
@@ -31,7 +24,6 @@ nbpCurrApp.controller("currDispCtrl", function ($scope, $http, $filter){
 				function (error) {
 					angular.element(document.querySelector('#info')).addClass('bg-danger');
 				});
-
 	};
 	$scope.getData(); // init function on startup
 });
