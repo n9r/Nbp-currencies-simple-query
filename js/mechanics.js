b5e31620b6d4d7587a5806a7ffb7413ee3c92185
n9r <no9org@gmail.com>
2016-08-03 13:43:23 +0200

    init commit

---
+++ js/mechanics.js
@@ -0,0 +1,37 @@
+var nbpCurrApp = angular.module("nbpCurrApp", []);
+
+nbpCurrApp.controller("currDispCtrl", function ($scope, $http, $filter){
+	$scope.readyMessage = "Working.";
+	$scope.nbpJson = "";
+	
+	$scope.fromDate = $filter('date')(new Date(),'yyyy-MM-dd');
+	$scope.toDate = $filter('date')(new Date(),'yyyy-MM-dd');
+
+	$scope.getData = function () {
+
+		angular.element(document.querySelector('#info')).addClass('bg-info');
+		
+		if ($scope.getUrl === undefined) {
+			$scope.getUrl = ""
+		} else {		
+			$scope.getUrl = "http://api.nbp.pl/api/exchangerates/tables/a/"+$scope.fromDate+"/"+$scope.toDate+"/?format=json";
+		}
+
+		$scope.promise = $http({method: "GET", url:	$scope.getUrl, responseType: "json"});
+		$scope.promise.then( function(data){
+		$scope.nbpJson = data;
+		});
+
+		$scope.promise
+			.then(
+				function(response) { 
+					$scope.nbpJson = response.data;
+					angular.element(document.querySelector('#info')).removeClass('bg-danger bg-info');
+					}, 
+				function (error) {	
+					angular.element(document.querySelector('#info')).addClass('bg-danger');
+				});
+
+	};
+	$scope.getData(); // init function on startup
+});
