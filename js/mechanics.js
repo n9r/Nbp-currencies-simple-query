8d248cd56fe98c7b14915524d22f69f6c39fcc3f
n9r <no9org@gmail.com>
2016-10-05 11:49:27 +0200

    replaced Bootstrap grid with table

--- js/mechanics.js
+++ js/mechanics.js
@@ -4,26 +4,24 @@ nbpCurrApp.controller("currDispCtrl", function ($scope, $http, $filter){
 	$scope.nbpJson = "";
 	$scope.fromDate = $filter('date')(new Date(),'yyyy-MM-dd');
 	$scope.toDate = $filter('date')(new Date(),'yyyy-MM-dd');
+
 	$scope.getData = function () {
 		angular.element(document.querySelector('#info')).addClass('bg-info');
 		if ($scope.getUrl === undefined) {
 			$scope.getUrl = ""
-		} else {		
+		} else {
 			$scope.getUrl = "http://api.nbp.pl/api/exchangerates/tables/a/"+$scope.fromDate+"/"+$scope.toDate+"/?format=json";
 		}
 		$scope.promise = $http({method: "GET", url:	$scope.getUrl, responseType: "json"});
 		$scope.promise.then( function(data){
-		$scope.nbpJson = data;
+			$scope.nbpJson = data;
 		});
-		$scope.promise
-			.then(
-				function(response) { 
-					$scope.nbpJson = response.data;
-					angular.element(document.querySelector('#info')).removeClass('bg-danger bg-info');
-					}, 
-				function (error) {
-					angular.element(document.querySelector('#info')).addClass('bg-danger');
-				});
+		$scope.promise.then(	function(response) { 
+			$scope.nbpJson = response.data;
+			angular.element(document.querySelector('#info')).removeClass('bg-danger bg-info');
+		}, function (error) {
+			angular.element(document.querySelector('#info')).addClass('bg-danger');
+		});
 	};
 	$scope.getData(); // init function on startup
 });
